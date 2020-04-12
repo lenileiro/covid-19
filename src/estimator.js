953f337e6430247a3dbf3bcf0e82b2f86273ad07
Anthony Leiro <lenileiro@gmail.com>
2020-04-12 18:34:48 +0300

    ch-1: finished challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,70 @@
-const covid19ImpactEstimator = (data) => data;
+const { chain } = require('on-covid-19');
 
-export default covid19ImpactEstimator;
+
+const getDays = (periodType, timeToElapse) => {
+  let days = 0;
+  if (periodType === 'DAYS' || periodType === 'days') {
+    days = timeToElapse;
+  } else if (periodType === 'WEEKS' || periodType === 'WEEK' || periodType === 'weeks' || periodType === 'week') {
+    days = timeToElapse * 7;
+  } else if (periodType === 'MONTHS' || periodType === 'MONTH' || periodType === 'months' || periodType === 'months') {
+    days = timeToElapse * 30;
+  }
+  return days;
+};
+
+const getTimeDuration = (periodType, timeToElapse) => {
+  const days = getDays(periodType, timeToElapse);
+  const projected = (2 ** Math.floor(days / 3));
+  return projected;
+};
+
+const estimateCurrentlyInfected = (input) => {
+    const severeImpact = {};
+    const impact = {};
+    const { data, data: { reportedCases } } = input;
+  
+    impact.currentlyInfected = reportedCases * 10;
+  
+    severeImpact.currentlyInfected = reportedCases * 50;
+  
+    return {
+      data,
+      impact,
+      severeImpact
+    };
+};
+
+const estimateProjectedInfections = (input) => {
+  const {
+    data, impact, severeImpact, data: { periodType, timeToElapse }
+  } = input;
+
+  const projected = getTimeDuration(periodType, timeToElapse);
+
+  impact.infectionsByRequestedTime = impact.currentlyInfected * projected;
+  severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * projected;
+  return {
+    data,
+    impact,
+    severeImpact
+  };
+};
+
+
+const covid19ImpactEstimator = (data) => {
+  const estimator = chain(
+
+    // challenge 1
+    estimateCurrentlyInfected,
+    estimateProjectedInfections
+  );
+
+  return estimator({
+    data,
+    impact: {},
+    severeImpact: {}
+  });
+};
+
+module.exports = covid19ImpactEstimator;
